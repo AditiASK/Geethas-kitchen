e38c9bd23c6634666617fc06491d7f58bbc27d4f
AditiASK <ariagic@yahoo.com>
2025-05-08 00:09:21 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("orderForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Order submitted! Please proceed to payment.");
-    window.location.href = "thankyou.html";
-  });
-  
