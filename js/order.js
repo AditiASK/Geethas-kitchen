05cc33cbd56dd3afa4e8158d082cb03b74299271
AditiASK <ariagic@yahoo.com>
2025-05-08 11:13:28 +0530

    Update order.js

--- js/order.js
+++ js/order.js
@@ -1,17 +1,21 @@
 const menuData = [
-    { name: "3 Poori + Aloo Palya + Chutney", price: 90 },
-    { name: "2 Plain Dosa + Chutney", price: 50 },
-    { name: "2 Karam Dosa + Chutney", price: 70 },
-    { name: "2 Ghee Karam Dosa + Chutney", price: 80 },
-    { name: "2 Masala Dosa + Chutney", price: 90 },
-    { name: "6 Paddu/Paniyaram + Chutney", price: 70 },
-    { name: "2 Egg Dosa + Chutney", price: 100 },
-    { name: "Rice + Andhra Pappu + Kabab (3 pcs)", price: 120 },
-    { name: "Rice + Andhra Pappu + Papad", price: 80 },
-    { name: "Chicken Kabab", price: 100 },
-    { name: "Chicken Fry", price: 100 },
-    { name: "Chicken Biryani (750ml) + Raita", price: 150 }
-  ];
+  { name: "3 Poori + Aloo Palya + Chutney", price: 90, image: "poori.jpg" },
+  { name: "2 Plain Dosa + Chutney", price: 50, image: "dosa.jpg" },
+  { name: "2 Karam Dosa + Chutney", price: 70, image: "dosa.jpg" },
+  { name: "2 Ghee Karam Dosa + Chutney", price: 80, image: "dosa.jpg" },
+  { name: "2 Masala Dosa + Chutney", price: 90, image: "dosa.jpg" },
+  { name: "6 Paddu/Paniyaram + Chutney", price: 70, image: "paddu.jpg" },
+  { name: "2 Egg Dosa + Chutney", price: 100, image: "dosa.jpg" },
+  { name: "Rice + Andhra Pappu + Pepper Chicken (3 pcs)", price: 120, image: "peppr-chicken.jpg" },
+  { name: "Rice + Andhra Pappu + Papad", price: 80, image: "lunch.jpg" },
+  { name: "Chicken Kabab", price: 100, image: "chicken-kebab.jpg" },
+  { name: "Chicken Fry", price: 100, image: "chicken-fry.jpg" },
+  { name: "Chicken Biryani (750ml) + Raita", price: 150, image: "chicken-biriyani.jpg" }
+  { name: "Rice + Andhra Pappu + Chicken Fry (3 pcs)", price: 120, image: "non-veg-1.jpg" },
+  { name: "Rice + Andhra Pappu + Chicken Kabab (3 pcs)", price: 120, image: "non-veg-2.jpg" },
+
+];
+
   
   let cart = {};
 
@@ -24,7 +28,8 @@ const menuData = [
     const card = document.createElement("div");
     card.classList.add("menu-card");
     card.innerHTML = `
-      <img src="images/dosa.jpg" alt="${item.name}">
+      <img src="images/${item.image}" alt="${item.name}">
+
       <h3>${item.name}</h3>
       <p>₹${item.price}</p>
       <button onclick="addToCart(${index})" class="hero-btn">Add</button>
